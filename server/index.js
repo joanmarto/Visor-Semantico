bb8b8b19b02114de9d70fbfb856e5fb9bc6fcadf
Joan Martorell <joanmartorell23@gmail.com>
2023-05-07 23:10:00 +0200

    prueba socket

--- server/index.js
+++ server/index.js
@@ -1,47 +1,22 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 const fs = require('fs');
+
 const app = express();
 const port = 80;
+
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.get('/', (req, res) => {
     res.sendFile('/home/gdie2305/Visor-Semantico/index.html');
 });
 
-app.listen(port, (requ, res) => {
+server.listen(port, (requ, res) => {
     console.log(`Prueba escuchando en puerto ${port}`);
 });
 
-/*
-app.post('/', (req, res) => {
-    console.log('Request: ' + req.body);
-    console.log('Response: ' + res.body);
-
-    //Leemos el JSON
-    
-    var data = fs.readFileSync('Visor-Semantico/json/quiz-chess_video.json');
-    var quiz = JSON.parse(data);
-    console.log(quiz);
-
-    //var newQuiz = req.body;
-    var newQuiz = {"question": "?",
-                    "answer": "yes"
-                };
-    //Añadimos el nuevo objeto
-    quiz.push(newQuiz);
-
-    //Escribimos en el JSON
-    fs.writeFileSync('Visor-Semantico/json/quiz-chess_video.json', JSON.stringify(quiz), (err) => {
-        //Error checking
-        if(err) throw err;
-        console.log("New quiz added");
-    });
-    
-    var server = {"server":"Success"};
-    //res.send(quiz);
-    res.send(server);
-    //res.send('OK');
-});
-*/
 app.use(express.json());
 
 app.post('/', (req, res) => {
@@ -64,6 +39,10 @@ app.post('/', (req, res) => {
     });
 
     res.send(req.body); // Responder con una respuesta de "OK"
+});
+
+io.on('connection', (socket) => {
+    console.log("Se ha conectado alguien");
 });
 
 app.use(express.static('/home/gdie2305/Visor-Semantico/'));
