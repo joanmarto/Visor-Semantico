2ef83c4d83164e7f338bded3ccf37b5bef98e1fc
Joan Martorell <joanmartorell23@gmail.com>
2023-05-08 10:58:31 +0200

    user names

--- js/chat.js
+++ js/chat.js
@@ -1,13 +1,23 @@
-const socket = io();
+//const socket = io();
 const messages = document.getElementById('message-chat');
 const btn = document.getElementById('send-msg-btn');
 const input = document.getElementById("chat-text-area");
 
+var userName;
+var loggedUser = false;
+
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log("enviando mensage: " + input.value);
+    //console.log("enviando mensage: " + input.value);
     if(input.value){
-        socket.emit('chat message', input.value);
+        if(loggedUser){
+            //console.log("Usuario -> " + userName);
+            //console.log(userName + ": " + input.value);
+            socket.emit('chat message', userName + ": " + input.value);
+        }else{
+            userName = getName(input.value);
+            loggedUser = true;
+        }
         input.value = '';
     }
 });
@@ -18,3 +28,7 @@ socket.on('chat message', (msg) => {
     messages.appendChild(item);
     window.scrollTo(0, messages.scrollHeight);
 });
+
+function getName(data){
+    return data.substring(data.indexOf(':') + 2, data.length);
+}
