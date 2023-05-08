606502afce634846e7320e73125ccaf3831353ab
Joan Martorell <joanmartorell23@gmail.com>
2023-05-08 09:33:17 +0200

    chat send buton events

--- js/chat.js
+++ js/chat.js
@@ -1,10 +1,11 @@
 const socket = io();
 const messages = document.getElementById('message-chat');
-const form = document.getElementById('form-chat-text-area');
+const btn = document.getElementById('send-msg-btn');
 const input = document.getElementById("chat-text-area");
 
-form.addEventListener('submit', (e) => {
+btn.addEventListener('click', (e) => {
     e.preventDefault();
+    console.log("enviando mensage")
     if(input.value){
         socket.emit('chat mesage', input.value);
         input.value = '';
